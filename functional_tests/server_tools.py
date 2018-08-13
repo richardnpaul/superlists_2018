2194163f43a59bd9a90e1286323cc02a3500afdf
Richard Paul <richardnpaul@gmail.com>
2018-08-13 09:19:34 +0100

    Custom changes because I'm using an internal server

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -1,5 +1,14 @@
+import os
+
 from fabric.api import run
 from fabric.context_managers import settings, shell_env
+
+server = os.environ.get('SERVER')
+port = os.environ.get('PORT')
+
+"""
+Run like this: SERVER=192.168.0.1 PORT=22 STAGING_SERVER=stage-superlists.richardnpaul.co.uk ./manage.py test functional_tests.test_my_lists
+"""
 
 
 def _get_manage_dot_py(host):
@@ -7,7 +16,7 @@ def _get_manage_dot_py(host):
 
 def reset_database(host):
     manage_dot_py = _get_manage_dot_py(host)
-    with settings(host_string=f'richard@{host}'):
+    with settings(host_string=f'richard@{server}:{port}'):
         run(f'{manage_dot_py} flush --noinput')
 
 def _get_server_env_vars(host):
@@ -16,7 +25,7 @@ def _get_server_env_vars(host):
 
 def create_session_on_server(host, email):
     manage_dot_py = _get_manage_dot_py(host)
-    with settings(host_string=f'richard@{host}'):
+    with settings(host_string=f'richard@{server}:{port}'):
         env_vars = _get_server_env_vars(host)
         with shell_env(**env_vars):
             session_key = run(f'{manage_dot_py} create_session {email}')
