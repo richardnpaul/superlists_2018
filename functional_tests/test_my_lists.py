2e0c12d00d95b2a23db6bddf6bf676668a0ce03e
Richard Paul <richardnpaul@gmail.com>
2018-08-18 11:10:13 +0100

    Remove duplicated code from ft

--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -22,16 +22,6 @@ class MyListsTest(FunctionalTest):
         ))
 
     def test_logged_in_users_lists_are_saved_as_my_lists(self):
-        email = 'edith@example.com'
-        self.browser.get(self.live_server_url)
-        self.wait_to_be_logged_out(email)
-
-        # Edith is a logged-in user
-        self.create_pre_authenticated_session(email)
-        self.browser.get(self.live_server_url)
-        self.wait_to_be_logged_in(email)
-
-    def test_logged_in_users_lists_are_saved_as_my_lists(self):
         # Edith is a logged-in user
         self.create_pre_authenticated_session('edith@example.com')
 
